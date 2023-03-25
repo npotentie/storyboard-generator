c09039eaabe8dfdcc05291afba90e4591f78aec1
Kasper Beljaars <kasperbeljaars@hotmail.com>
2023-03-25 09:36:30 +0100

    Created Footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,29 @@
+import Link from "next/link";
+import Logo from "./Logo";
+
+const Navbar = () => {
+    let logoProps = {
+        width:"80",
+        height:"80"
+    }
+
+    return (
+        <>
+        <footer>
+        </footer>
+        <style jsx>{`
+            footer {
+                position: sticky;
+                z-index: 30;
+                bottom: 0;
+
+                height: 104px; // ONLY FOR FILL UP
+                padding: 10px 130px;
+                background-color: #000;
+            }
+        `}</style>
+        </>
+    );
+};
+
+export default Navbar;
