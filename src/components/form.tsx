0554fbcca082241e509c903c00ee178489a80c5a
Louis Laureys <louis@laureys.me>
2023-04-03 01:24:36 +0200

    For loops exist

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -4,19 +4,9 @@ import Question from "./question";
 import questions from "../questions.json";
 
 const Form = () => {
-    const [scene, setScene] = useState({
-        setting: '',
-        camera: '',
-        emotions: '',
-        details: '',
-        lighting: '',
-        colors: '',
-        weather: '',
-        props: '',
-        composition: '',
-        characters: '',
-        clothing: '',
-    });
+    const [scene, setScene] = useState(
+      Object.fromEntries(Object.entries(questions).map(([questionKey]) => [questionKey, ""]))
+    );
     const [isLoading, setLoading] = useState(false);
 
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -62,85 +52,16 @@ const Form = () => {
     return (
       
         <form onSubmit={handleSubmit}>
-
-          <Question
-            question={questions.setting.question}
-            description={questions.setting.description}
-            name="setting"
-            maxLength={questions.setting.limit}
-            onChange={handleChange}
-          />
-          <Question
-            question={questions.camera.question}
-            description={questions.camera.description}
-            name="camera"
-            maxLength={questions.camera.limit}
-            onChange={handleChange}
-          />
-          <Question
-            question={questions.emotions.question}
-            description={questions.emotions.description}
-            name="emotions"
-            maxLength={questions.emotions.limit}
-            onChange={handleChange}
-          />
-          <Question
-            question={questions.details.question}
-            description={questions.details.description}
-            name="details"
-            maxLength={questions.details.limit}
-            onChange={handleChange}
-          />
-          <Question
-            question={questions.lighting.question}
-            description={questions.lighting.description}
-            name="lighting"
-            maxLength={questions.lighting.limit}
-            onChange={handleChange}
-          />
-          <Question
-            question={questions.colors.question}
-            description={questions.colors.description}
-            name="colors"
-            maxLength={questions.colors.limit}
-            onChange={handleChange}
-            
-          />
-          <Question
-            question={questions.weather.question}
-            description={questions.weather.description}
-            name="weather"
-            maxLength={questions.weather.limit}
-            onChange={handleChange}
-          />
-          <Question
-            question={questions.props.question}
-            description={questions.props.description}
-            name="props"
-            maxLength={questions.props.limit}
-            onChange={handleChange}
-          />
-          <Question
-            question={questions.composition.question}
-            description={questions.composition.description}
-            name="composition"
-            maxLength={questions.composition.limit}
-            onChange={handleChange}
-          />
-          <Question
-            question={questions.characters.question}
-            description={questions.characters.description}
-            name="characters"
-            maxLength={questions.characters.limit}
-            onChange={handleChange}
-          />
-          <Question
-            question={questions.clothing.question}
-            description={questions.clothing.description}
-            name="clothing"
-            maxLength={questions.clothing.limit}
-            onChange={handleChange}
-          />
+          {Object.entries(questions).map(([questionKey, question]) => (
+            <Question
+              key={questionKey}
+              question={question.question}
+              description={question.description}
+              name={questionKey}
+              maxLength={question.limit}
+              onChange={handleChange}
+            />
+          ))}
           <div className="container">
             <button type="submit" className="button submit-button" disabled={isLoading}>
               {isLoading ? (
