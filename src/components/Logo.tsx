e526dbd14d038a29e920214ff32042e70674c6bb
Louis Laureys <louis@laureys.me>
2023-04-03 01:37:55 +0200

    Fix logo src

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 const Logo = ({ width, height }: { width: number; height: number }) => {
   return (
     <Image
-      src="/../public/logo.png"
+      src="/logo.png"
       alt="Logo NPO"
       width={width}
       height={height}
